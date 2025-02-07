0012ad0a72a73cb975c05c8884778fe9cc86ded1
uschrono <junin_gym@hotmail.com>
2025-02-07 10:31:57 -0300

    First commit

---
+++ cypress/e2e/Login.cy.js
@@ -0,0 +1,19 @@
+describe('Orange HRM Test', () => {
+  it('Login - Sucess', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get("[name='username']").type('Admin')
+    cy.get("[name='password']").type('admin123')
+    cy.get("[type='submit']").click()
+    cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
+    cy.get('.oxd-topbar-header-breadcrumb-module').contains('Dashboard')
+
+  })
+  it('Login - Fail', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get("[name='username']").type('Test')
+    cy.get("[name='password']").type('test')
+    cy.get("[type='submit']").click()
+    cy.get('.oxd-alert')
+
+  })
+})
