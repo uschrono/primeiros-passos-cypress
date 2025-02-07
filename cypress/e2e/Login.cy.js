c3adcf75235aa0a5ca66e1d7bcaf3348ab975f8e
uschrono <junin_gym@hotmail.com>
2025-02-07 12:18:17 -0300

    base url added

--- cypress/e2e/Login.cy.js
+++ cypress/e2e/Login.cy.js
@@ -12,8 +12,8 @@ describe('Orange HRM Test', () => {
   }
 
   it('Login - Sucess', () => {
-    
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+
+    cy.visit('/auth/login')
     cy.get(selectorsList.usernameField).type(userData.userSucess.username)
     cy.get(selectorsList.passwordField).type(userData.userSucess.password)
     cy.get(selectorsList.loginButton).click()
@@ -21,7 +21,7 @@ describe('Orange HRM Test', () => {
     cy.get(selectorsList.sectionTitleTopBar).contains('Dashboard')
   })
   it('Login - Fail', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.visit('/auth/login')
     cy.get(selectorsList.usernameField).type(userData.userFail.username)
     cy.get(selectorsList.passwordField).type(userData.userFail.password)
     cy.get(selectorsList.loginButton).click()
